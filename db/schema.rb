1647f701a19b846028d6e0125c3a9a582a225472
jleewebdev <lee45276@gmail.com>
2016-05-03 22:05:39 +0800

    Add videos to home page

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160503132514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "videos", force: true do |t|
+    t.string "title"
+    t.string "description"
+    t.string "small_cover_url"
+    t.string "large_cover_url"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+[
+  {
+  title: "Family Guy Season 1",
+  description: "This is season one",
+  small_cover_url: "family_guy.jpg",
+  large_cover_url: "family_guy.jpg"
+  },
+{
+  title: "Futurama",
+  description: "This is season one",
+  small_cover_url: "futurama.jpg",
+  large_cover_url: "futurama.jpg"
+  },
+{
+  title: "Monk",
+  description: "This is season one",
+  small_cover_url: "monk.jpg",
+  large_cover_url: "monk_large.jpg"
+  },
+{
+  title: "South Park",
+  description: "This is season one",
+  small_cover_url: "south_park.jpg",
+  large_cover_url: "south_park.jpg"
+  }
+].each do |movie|
+  Video.create(movie)
+end
