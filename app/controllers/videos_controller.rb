9ce92b9095f649d717e98baee935f0533caa221a
jleewebdev <lee45276@gmail.com>
2016-05-03 22:15:10 +0800

    Add show page , not all functional

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,19 @@
 class VideosController < ApplicationController
 
+  before_action :set_video, only: [:show]
+
   def index
     @videos = Video.all
+  end
+
+  def show
+    
+  end
+
+  private
+
+  def set_video
+    @video = Video.find(params[:id])
   end
 
 end
