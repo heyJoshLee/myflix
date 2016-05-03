d4547f2ff7db620796c1fb3ba215803ac658bcf5
jleewebdev <lee45276@gmail.com>
2016-05-03 22:44:35 +0800

    Add categories model, controller , show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,31 +6,49 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+[
+  {
+    name: "funny"
+  },
+  {
+    name: "cartoon"
+  }
+].each do  |category|
+  Category.create(category)
+end
+
+
 
 [
   {
   title: "Family Guy Season 1",
   description: "This is season one",
   small_cover_url: "family_guy.jpg",
-  large_cover_url: "family_guy.jpg"
+  large_cover_url: "family_guy.jpg",
+  category_id: 1
   },
 {
   title: "Futurama",
   description: "This is season one",
   small_cover_url: "futurama.jpg",
-  large_cover_url: "futurama.jpg"
+  large_cover_url: "futurama.jpg",
+  category_id: 2
+
   },
 {
   title: "Monk",
   description: "This is season one",
   small_cover_url: "monk.jpg",
-  large_cover_url: "monk_large.jpg"
+  large_cover_url: "monk_large.jpg",
+  category_id: 1
+
   },
 {
   title: "South Park",
   description: "This is season one",
   small_cover_url: "south_park.jpg",
-  large_cover_url: "south_park.jpg"
+  large_cover_url: "south_park.jpg",
+  category_id: 2
   }
 ].each do |movie|
   Video.create(movie)
