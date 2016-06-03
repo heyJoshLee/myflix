17c54b18cee86ac8ce7955b3df24eb4654564f84
jleewebdev <lee45276@gmail.com>
2016-06-03 21:48:16 +0800

    Fix feature test

--- spec/features/admin_adds_new_video_spec.rb
+++ spec/features/admin_adds_new_video_spec.rb
@@ -12,15 +12,15 @@ feature "Admin adds new video" do
     fill_in "Description", with: "SF detective"
     attach_file "Large cover", "spec/support/uploads/monk_large.jpg"
     attach_file "Small cover", "spec/support/uploads/monk.jpg"
-    fill_in "Video URL", with: "http://www.example.com/my_video.mp4"
+    fill_in "Url", with: "http://www.example.com/my_video.mp4"
     click_button "Add Video"
 
     sign_out
     sign_in
 
     visit video_path(Video.first)
-    expect(page).to have_selector("img[src'uploads/monk_large.jpg]")
-    expect(page).to have_selector("a[href='http://www.example.com/my_video/mp4")
+    expect(page).to have_selector("img[src='/spec/monk_large.jpg']")
+    expect(page).to have_selector("a[href='http://www.example.com/my_video.mp4']")
 
 
   end
