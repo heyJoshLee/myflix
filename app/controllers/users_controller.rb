334c7bc532a545cb81b3f673f47ba4b7a1d9a556
jleewebdev <lee45276@gmail.com>
2016-06-17 15:26:21 +0800

    Add stripe wrapper and stripe wrapper tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,18 +23,23 @@ def new_with_invitation
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      handle_invitaiton
-      Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+    if @user.valid?
       
-      StripeWrapper::Charge.create(
+      charge = StripeWrapper::Charge.create(
         amount: 999,
         card: params[:stripeToken],
         description: "Sign up charge for #{@user.email}"
       )
-      AppMailer.send_welcome_email(@user).deliver
-      flash[:notice] = "Your account was created"
-      redirect_to videos_path
+      if charge.successful?
+        @user.save
+        handle_invitaiton
+        AppMailer.send_welcome_email(@user).deliver
+        flash[:notice] = "Your account was created"
+        redirect_to videos_path
+      else
+        flash[:error] = charge.error_message
+        render :new
+      end
     else
       render :new
     end
