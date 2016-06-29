aeab39ea647ce33dd6bfaf23f9ea9b7265702434
jleewebdev <lee45276@gmail.com>
2016-06-29 18:36:10 +0800

    Add advanced search function. One feature test and one video_model test don't pass. The search function shuold be on a different branch, but I messed up

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ActiveRecord::Base
 
-  belongs_to :video
+  belongs_to :video, touch: true
   belongs_to :user
 
   validates_presence_of :content, :rating
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Myflix::Application.routes.draw do
 
     collection do
       get "search", to: "videos#search"
+      get "advanced_search", to: "videos#advanced_search", as: :advanced_search
     end
     resources :reviews, only: [:create]
   end
